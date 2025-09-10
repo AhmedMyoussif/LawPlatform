21479e69afccfe015bd3164264483848b75f0fb2
AhmedMyoussif <ahmedhamouda18hhh@gmail.com>
2025-09-11 01:32:07 +0300

    add column isdeleted , description for category

--- LawPlatform.Entities/Models/ConsultationCategory.cs
+++ LawPlatform.Entities/Models/ConsultationCategory.cs
@@ -10,6 +10,9 @@ namespace LawPlatform.Entities.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
 
         public ICollection<Consultation> Consultations { get; set; } = new List<Consultation>();
     }
