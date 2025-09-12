4029131c48533e2bfe77b7e22c61eecb3dcca4f7
AhmedMyoussif <ahmedhamouda18hhh@gmail.com>
2025-09-13 01:47:47 +0300

    SpecializationEnum

---
+++ LawPlatform.Utilities/Enums/Specialization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawPlatform.Utilities.Enums
+{
+    public enum Specialization
+    {
+        CivilLaw,
+        CriminalLaw,
+        FamilyLaw,
+        CommercialLaw,
+        LaborLaw,
+        AdministrativeLaw,
+        RealEstateLaw,
+        ConstitutionalLaw,
+        InternationalLaw,
+        IntellectualPropertyLaw
+    }
+}
